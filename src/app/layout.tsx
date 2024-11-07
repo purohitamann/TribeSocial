1b864bfcfb9c66ef4a696e8f12d391534c28f5b2
Aman Purohit <purohiam@sheridancollege.ca>
2024-11-06 20:17:57 -0500

    Init Clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ConvexClientProvider } from "./convex-client-provider";
 
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 export const metadata: Metadata = {
@@ -20,11 +21,15 @@ export default function RootLayout({
       <body
 
       >
-        <ConvexClientProvider>
+        <ClerkProvider dynamic>
+
+          <ConvexClientProvider>
 
 
-          {children}
-        </ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
+
+        </ClerkProvider>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 'use client';
-import { useQuery, useMutation } from "convex/react";
+import { Authenticated, useQuery, useMutation, Unauthenticated } from "convex/react";
 import { useState } from "react";
 import { api } from "../../convex/_generated/api";
+import { SignInButton } from "@clerk/nextjs";
+
 // {api} or api? api is a variable, not a function
 // interface Messages {
 //   sender: string;
@@ -22,20 +24,30 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">Welcome to Tribes</h1>
-      <div>
-        {messages?.map((message, key) => (
-          <div key={key} className="flex gap-2">
-            <div className="font-bold">{message.sender}:</div>
-            <div>{message.content}</div>
+    <>
+
+      <Authenticated >
+
+
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+          <h1 className="text-4xl font-bold">Welcome to Tribes</h1>
+          <div>
+            {messages?.map((message, key) => (
+              <div key={key} className="flex gap-2">
+                <div className="font-bold">{message.sender}:</div>
+                <div>{message.content}</div>
+              </div>
+            ))}
+            <form onSubmit={sendMessage} className="flex gap-2 bg-[#f2f2f2] p-2">
+              <input className="w-full border-black" type="text" name="message" id="message" onChange={e => setInput(e.target.value)} />
+              <button className="bg-black text-white px-4 py-2 rounded">Send</button>
+            </form>
           </div>
-        ))}
-        <form onSubmit={sendMessage} className="flex gap-2 bg-[#f2f2f2] p-2">
-          <input className="w-full border-black" type="text" name="message" id="message" onChange={e => setInput(e.target.value)} />
-          <button className="bg-black text-white px-4 py-2 rounded">Send</button>
-        </form>
-      </div>
-    </div>
+        </div>
+      </Authenticated>
+      <Unauthenticated>
+        <SignInButton />
+      </Unauthenticated>
+    </>
   );
 }
